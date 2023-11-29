2271ef576c56a5a131e8b81517bf31dd63c1c183
dontrellthedeveloper <dontrellknight@gmail.com>
2023-11-29 14:43:39 -0500

    added homepage schema

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,5 +1,6 @@
 import test from "./test"
 import survey from "./survey"
 import form from "./form"
+import homepage from "./homepage"
 
-export const schemaTypes = [test, survey, form]
+export const schemaTypes = [test, survey, form, homepage]
