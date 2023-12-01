73170279f70ba3a3a718264a5948ff53643aef6d
dontrellthedeveloper <dontrellknight@gmail.com>
2023-11-30 23:05:23 -0500

    added contact schema

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -2,5 +2,6 @@ import test from "./test"
 import survey from "./survey"
 import form from "./form"
 import homepage from "./homepage"
+import contact from "./contact"
 
-export const schemaTypes = [test, survey, form, homepage]
+export const schemaTypes = [test, survey, form, homepage, contact]
