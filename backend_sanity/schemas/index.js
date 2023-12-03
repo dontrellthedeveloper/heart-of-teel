a5dc681de21f0311332ffb0f783de6102daa6bcc
dontrellthedeveloper <dontrellknight@gmail.com>
2023-12-03 16:53:01 -0500

    added gallery schema

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -3,5 +3,7 @@ import survey from "./survey"
 import form from "./form"
 import homepage from "./homepage"
 import contact from "./contact"
+import donate from "./donate"
+import gallery from "./gallery"
 
-export const schemaTypes = [test, survey, form, homepage, contact]
+export const schemaTypes = [test, survey, form, homepage, contact, donate, gallery]
